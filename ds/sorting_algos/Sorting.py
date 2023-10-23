fa934a03ea0afcaa2984a86097095ddd60d95e3b
PranjalDby <dpranjal.088@gmail.com>
2023-10-23 11:12:05 +0530

    hello

--- ds/sorting_algos/Sorting.py
+++ ds/sorting_algos/Sorting.py
@@ -28,7 +28,6 @@ def selection_sort(array):
         arr[i] = arr[small]
         arr[small] = temp
             
-
     return array
 
 def quick_sort(arr,low,high):
@@ -39,6 +38,45 @@ def quick_sort(arr,low,high):
         quick_sort(arr,pivot+1,high)
     
     return
+
+
+def divide(arr,low,high):
+
+    if low < high:
+        mid = (high + low) // 2
+        divide(arr,low,mid)
+        divide(arr,mid+1,high)
+        merge(arr,low,high,mid)
+
+def merge(arr,low,high,mid):
+    sorted = []
+    for i in range(high-low+1):
+        sorted.append(i)
+    x = low
+    y = mid+1
+    z = 0
+    while(x<=mid and y <=high):
+        if arr[x] < arr[y]:
+            sorted[z] = arr[x]
+            z+=1
+            x+=1
+        
+        else:
+            sorted[z] = arr[y]
+            z+=1
+            y+=1
+    
+    while x <= mid:
+        sorted[z] = arr[x]
+        z+=1
+        x+=1
+
+    while y <= high:
+        sorted[z] = arr[y]
+        z+=1
+        y+=1
+    for i in range(low,high+1):
+        arr[i] = sorted[i-low]
 
 def get_pivot(arr,low,high):
     pivot = arr[high]
@@ -55,19 +93,22 @@ def get_pivot(arr,low,high):
     arr[high] = temp
 
     return i+1
-arr = [1,2,4,6,5,3,8,7]
+arr1 = [1,2,4,6,5,3,8,7]
 
-start1 = timeit.default_timer()
-arr = selection_sort(arr)
-end1 = timeit.default_timer()
-print("total time taken by selection sort",end1-start1)
 
-arr = [5,2,4,6,1,3,8,7,11,14]
+arr = [5,2,4,6,8,1]
 
 # print(insertion_sort(arr))
 start = timeit.default_timer()
-high = len(arr)-1
-quick_sort(arr,0,high)
-end = timeit.default_timer()
-print("Total time to execute..",end-start)
+# high = len(arr)-1
+# start1 = timeit.default_timer()
+# # arr = selection_sort(arr)
+# end1 = timeit.default_timer()
+
+# print("time = ",end1-start1)
+# # quick_sort(arr,0,high)
+# end = timeit.default_timer()
+# print("Total time to execute..",end-start)
+# print(arr)
+divide(arr,0,len(arr)-1)
 print(arr)
